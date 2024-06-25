481138978a021310a5d6d488faaed362dbcce48c
Ki-jong Song <jjongs212@gmail.com>
2024-06-25 18:41:27 +0900

    Time: 803 ms (49.33%), Space: 19.4 MB (98%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
@@ -1,18 +1,15 @@
-from collections import deque
-
-
 class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
-        nums_length = len(nums)
-        flip_count = 0
-        flip_queue = deque()
+        length = len(nums)
+        flips = [0 for _ in range(length)]
+        flip = 0
         for i, num in enumerate(nums):
-            if flip_queue and flip_queue[0] + k == i:
-                flip_queue.popleft()
-            if (num + len(flip_queue)) & 1 == 1:
+            if i - k >= 0:
+                flip ^= flips[i - k]
+            if num != flip:
                 continue
-            if i + k > nums_length:
+            if i + k > length:
                 return -1
-            flip_queue.append(i)
-            flip_count += 1
-        return flip_count
+            flip ^= 1
+            flips[i] = 1
+        return sum(flips)
