c760237782239c2a98dcf064ee11b020ca49fb3d
Ki-jong Song <jjongs212@gmail.com>
2024-06-28 17:45:58 +0900

    Time: 49 ms (69.24%), Space: 48.8 MB (51.3%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -0,0 +1,53 @@
+
+var MyQueue = function () {
+    this.pushStack = [];
+    this.popStack = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+    this.pushStack.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+    if (this.popStack.length === 0) {
+        while (this.pushStack.length > 0) {
+            this.popStack.push(this.pushStack.pop());
+        }
+    }
+    return this.popStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+    if (this.popStack.length === 0) {
+        while (this.pushStack.length > 0) {
+            this.popStack.push(this.pushStack.pop());
+        }
+    }
+    return this.popStack.at(-1);
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+    return this.pushStack.length === 0 && this.popStack.length === 0;
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
