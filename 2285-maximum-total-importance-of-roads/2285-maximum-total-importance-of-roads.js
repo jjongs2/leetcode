1fa4c04e84e6accdacecf43a769ad396f88867a3
Ki-jong Song <jjongs212@gmail.com>
2024-06-28 18:59:25 +0900

    Time: 144 ms (100%), Space: 72.8 MB (92%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.js
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.js
@@ -4,15 +4,12 @@
  * @return {number}
  */
 var maximumImportance = function (n, roads) {
-    const degrees = new Array(n).fill(0);
-    for (const [a, b] of roads) {
-        degrees[a] += 1;
-        degrees[b] += 1
-    }
-    let totalImportance = 0;
-    degrees.sort((d1, d2) => d2 - d1);
-    degrees.forEach((degree, index) => {
-        totalImportance += degree * (n - index);
-    });
-    return totalImportance;
+    return roads
+        .reduce((degs, [a, b]) => {
+            degs[a] += 1;
+            degs[b] += 1;
+            return degs;
+        }, Array(n).fill(0))
+        .sort((deg1, deg2) => deg2 - deg1)
+        .reduce((result, deg, i) => result + deg * (n - i), 0);
 };
