0f7f69125270a3f45c1d4b54ccfb649349f7d42a
Ki-jong Song <jjongs212@gmail.com>
2024-06-29 06:40:28 +0900

    Time: 70 ms (33.35%), Space: 54.6 MB (98.82%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function (root) {
+    let diameter = 0;
+
+    const depth = (node) => {
+        if (!node) return 0;
+        const leftDepth = depth(node.left);
+        const rightDepth = depth(node.right);
+        diameter = Math.max(diameter, leftDepth + rightDepth);
+        return Math.max(leftDepth, rightDepth) + 1;
+    };
+
+    depth(root);
+    return diameter;
+};
