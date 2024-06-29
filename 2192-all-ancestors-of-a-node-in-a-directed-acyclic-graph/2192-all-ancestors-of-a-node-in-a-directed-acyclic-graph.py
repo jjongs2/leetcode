7c78187536c06aa49e97fba171d2c00456b054e3
Ki-jong Song <jjongs212@gmail.com>
2024-06-29 14:57:22 +0900

    Time: 481 ms (62.2%), Space: 45.7 MB (52.71%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,18 +1,15 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        adj = [[] for _ in range(n)]
+        answer = [set() for _ in range(n)]
         for v1, v2 in edges:
-            adj[v2].append(v1)
-        answer = [[] for _ in range(n)]
-        for i in range(n):
-            stack = [i]
-            visited = set()
+            answer[v2].add(v1)
+        for i, ancestors in enumerate(answer):
+            stack = list(ancestors)
             while stack:
-                v0 = stack.pop()
-                for v in adj[v0]:
-                    if v in visited:
-                        continue
-                    stack.append(v)
-                    visited.add(v)
-            answer[i] = sorted(visited)
-        return answer
+                v = stack.pop()
+                if v > i:
+                    for u in answer[v]:
+                        if u not in ancestors:
+                            stack.append(u)
+                ancestors |= answer[v]
+        return [sorted(ancestors) for ancestors in answer]
