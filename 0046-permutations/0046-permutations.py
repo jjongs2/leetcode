5e152b0af707a0d23f6364e40dc8dc8130efcdd5
Ki-jong Song <jjongs212@gmail.com>
2024-07-09 11:55:42 +0900

    Time: 43 ms (40.66%), Space: 16.8 MB (6.3%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,6 +1,16 @@
-from itertools import permutations
-
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return list(map(list, permutations(nums)))
+        result = []
+        num_count = len(nums)
+
+        def backtrack(index):
+            if index == num_count:
+                result.append(nums.copy())
+                return
+            for i in range(index, num_count):
+                nums[index], nums[i] = nums[i], nums[index]
+                backtrack(index + 1)
+                nums[index], nums[i] = nums[i], nums[index]
+
+        backtrack(0)
+        return result
