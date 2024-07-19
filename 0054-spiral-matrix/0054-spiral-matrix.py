a7e6a54bfdbd180294ead075fb31f47343aa8cbc
Ki-jong Song <jjongs212@gmail.com>
2024-07-19 11:54:02 +0900

    Time: 30 ms (88.07%), Space: 16.6 MB (33.21%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,21 +1,17 @@
+from itertools import chain
+
+
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        visited = []
-        top, bottom = 0, len(matrix) - 1
-        left, right = 0, len(matrix[0]) - 1
-        while top <= bottom and left <= right:
-            for c in range(left, right + 1):
-                visited.append(matrix[top][c])
-            top += 1
-            for r in range(top, bottom + 1):
-                visited.append(matrix[r][right])
-            right -= 1
-            if top > bottom or left > right:
-                break
-            for c in range(right, left - 1, -1):
-                visited.append(matrix[bottom][c])
-            bottom -= 1
-            for r in range(bottom, top - 1, -1):
-                visited.append(matrix[r][left])
-            left += 1
-        return visited
+        if not matrix:
+            return []
+        m, n = len(matrix), len(matrix[0])
+        if m <= 1 or n <= 1:
+            return [*chain.from_iterable(matrix)]
+        return (
+            matrix[0][:-1]
+            + [matrix[r][-1] for r in range(m - 1)]
+            + [matrix[-1][c] for c in range(n - 1, 0, -1)]
+            + [matrix[r][0] for r in range(m - 1, 0, -1)]
+            + self.spiralOrder([row[1 : n - 1] for row in matrix[1 : m - 1]])
+        )
