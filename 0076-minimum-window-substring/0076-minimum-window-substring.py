9a3668691c4843ef143db76e4bbacbf523ba6c05
Ki-jong Song <jjongs212@gmail.com>
2024-08-12 14:10:56 +0900

    Time: 125 ms (52.5%), Space: 17.3 MB (21.09%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -6,6 +6,7 @@ class Solution:
         if len(s) < len(t):
             return ""
         t_counter, window_counter = Counter(t), Counter()
+        count, required = 0, len(t_counter)
         start, end = 0, -1
         min_size = len(s)
         left = 0
@@ -13,13 +14,18 @@ class Solution:
             if char not in t_counter:
                 continue
             window_counter[char] += 1
-            while window_counter >= t_counter:
+            if window_counter[char] == t_counter[char]:
+                count += 1
+            while count == required:
                 while s[left] not in t_counter:
                     left += 1
                 size = right - left
                 if size < min_size:
                     min_size = size
                     start, end = left, right
-                window_counter[s[left]] -= 1
+                char = s[left]
+                window_counter[char] -= 1
+                if window_counter[char] < t_counter[char]:
+                    count -= 1
                 left += 1
         return s[start : end + 1]
