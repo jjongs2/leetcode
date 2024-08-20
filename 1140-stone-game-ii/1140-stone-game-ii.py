f59ee1b72259f8cc2cd2c12d109aa8aac1bf49cb
Ki-jong Song <jjongs212@gmail.com>
2024-08-20 17:41:14 +0900

    Time: 132 ms (95.51%), Space: 18.1 MB (67.75%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,22 +1,21 @@
+from functools import lru_cache
+
+
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         n = len(piles)
         suffix_sums = [0 for _ in range(n + 1)]
         for i in range(n - 1, -1, -1):
             suffix_sums[i] = piles[i] + suffix_sums[i + 1]
-        memo = dict()
 
+        @lru_cache(maxsize=None)
         def take_stones(i, m):
             if i >= n:
                 return 0
             if i + 2 * m >= n:
                 return suffix_sums[i]
-            if (i, m) in memo:
-                return memo[(i, m)]
-            max_count = suffix_sums[i] - min(
+            return suffix_sums[i] - min(
                 take_stones(i + x, max(m, x)) for x in range(1, 2 * m + 1)
             )
-            memo[(i, m)] = max_count
-            return max_count
 
         return take_stones(0, 1)
