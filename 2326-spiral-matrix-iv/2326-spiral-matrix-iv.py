ece0c294e6121b283736d4cd5273fa34174dd8a3
Ki-jong Song <jjongs212@gmail.com>
2024-09-09 17:04:16 +0900

    Time: 684 ms (49.71%), Space: 56.7 MB (12.03%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -3,21 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-DIRECTIONS = ((0, 1), (1, 0), (0, -1), (-1, 0))
-
-
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         matrix = [[-1 for _ in range(n)] for _ in range(m)]
-        r, c = 0, 0
-        direction = 0
-        dr, dc = DIRECTIONS[direction]
-        while head:
-            matrix[r][c] = head.val
-            head = head.next
-            r2, c2 = r + dr, c + dc
-            if not (0 <= r2 < m and 0 <= c2 < n) or matrix[r2][c2] != -1:
-                direction = (direction + 1) % 4
-                dr, dc = DIRECTIONS[direction]
-            r, c = r + dr, c + dc
-        return matrix
+        top, left, bottom, right = 0, 0, m - 1, n - 1
+        while True:
+            for c in range(left, right + 1):
+                matrix[top][c] = head.val
+                if not (head := head.next):
+                    return matrix
+            top += 1
+            for r in range(top, bottom + 1):
+                matrix[r][right] = head.val
+                if not (head := head.next):
+                    return matrix
+            right -= 1
+            for c in range(right, left - 1, -1):
+                matrix[bottom][c] = head.val
+                if not (head := head.next):
+                    return matrix
+            bottom -= 1
+            for r in range(bottom, top - 1, -1):
+                matrix[r][left] = head.val
+                if not (head := head.next):
+                    return matrix
+            left += 1
