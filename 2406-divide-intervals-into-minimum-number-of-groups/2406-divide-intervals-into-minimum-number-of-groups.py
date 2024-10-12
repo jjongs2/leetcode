6140d969c664959695413f4f5fe23c7dd2e84d13
Ki-jong Song <jjongs212@gmail.com>
2024-10-12 16:38:15 +0900

    Time: 1106 ms (59.07%), Space: 55.7 MB (98.31%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -1,15 +1,12 @@
-from collections import defaultdict
+from heapq import heappush, heapreplace
 
 
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
-        counts = defaultdict(int)
-        for left, right in intervals:
-            counts[left] += 1
-            counts[right + 1] -= 1
-        result = 0
-        count_sum = 0
-        for _, count in sorted(counts.items()):
-            count_sum += count
-            result = max(result, count_sum)
-        return result
+        heap = []
+        for left, right in sorted(intervals):
+            if heap and heap[0] < left:
+                heapreplace(heap, right)
+            else:
+                heappush(heap, right)
+        return len(heap)
