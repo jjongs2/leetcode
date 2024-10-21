fcedbc77c8198553d9cfac4dede519b760fa9b67
Ki-jong Song <jjongs212@gmail.com>
2024-10-21 19:49:36 +0900

    Time: 15 ms (98.43%), Space: 16.7 MB (6.28%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.py
@@ -1,8 +1,9 @@
 class Solution:
     def maxUniqueSplit(self, s: str) -> int:
         n = len(s)
+        substrings = set()
 
-        def backtrack(start, substrings):
+        def backtrack(start):
             if start == n:
                 return len(substrings)
             max_count = 0
@@ -11,7 +12,9 @@ class Solution:
                     continue
                 if len(substrings) + n - end < max_count:
                     break
-                max_count = max(max_count, backtrack(end, substrings | {substring}))
+                substrings.add(substring)
+                max_count = max(max_count, backtrack(end))
+                substrings.remove(substring)
             return max_count
 
-        return backtrack(0, set())
+        return backtrack(0)
