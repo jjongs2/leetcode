2433a7df2283bbc4f870be693fd8da4d0df2d2c0
Ki-jong Song <jjongs212@gmail.com>
2024-11-02 14:40:52 +0900

    Time: 0 ms (100%), Space: 16.5 MB (80.88%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        for i, char in enumerate(sentence):
+            if char != " ":
+                continue
+            if sentence[i - 1] != sentence[i + 1]:
+                return False
+        return sentence[0] == sentence[-1]
