b9c22d0fc21625a421c17c064d2415eea7a0d75c
Ki-jong Song <jjongs212@gmail.com>
2024-12-04 14:14:09 +0900

    Time: 43 ms (97.92%), Space: 17.5 MB (50.24%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -1,12 +1,10 @@
 class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
-        n = len(str1)
-        s = tuple(map(ord, str1))
+        n = len(str2)
         i = 0
-        for c in map(ord, str2):
-            while i < n and c - s[i] not in (-25, 0, 1):
+        for c in str1:
+            if (ord(str2[i]) - ord(c)) % 26 <= 1:
                 i += 1
-            if i == n:
-                return False
-            i += 1
-        return True
+                if i == n:
+                    return True
+        return False
