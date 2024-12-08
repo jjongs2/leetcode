e67dc9e23359fa7cde6c96c67411ab9c1a525ad4
Ki-jong Song <jjongs212@gmail.com>
2024-12-08 21:47:04 +0900

    Time: 97 ms (96.6%), Space: 55.5 MB (92.8%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
@@ -1,22 +1,15 @@
-from math import inf
-from sortedcontainers import SortedDict
+from bisect import bisect_left
 
 
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
-        max_to, max_from = dict(), SortedDict({inf: 0})
-        max_v = 0
-        events.sort(key=lambda x: x[1])
-        for _, e, v in events:
-            max_v = max(max_v, v)
-            max_to[e] = max_v
-        max_v = 0
-        events.sort(reverse=True)
-        for s, _, v in events:
-            max_v = max(max_v, v)
-            max_from[s] = max_v
         result = 0
-        for e, v1 in max_to.items():
-            _, v2 = max_from.peekitem(max_from.bisect_right(e))
-            result = max(result, v1 + v2)
+        max_ends, max_values = [0], [0]
+        events.sort(key=lambda x: x[1])
+        for s, e, v in events:
+            i = bisect_left(max_ends, s) - 1
+            result = max(result, max_values[i] + v)
+            if v > max_values[-1]:
+                max_ends.append(e)
+                max_values.append(v)
         return result
