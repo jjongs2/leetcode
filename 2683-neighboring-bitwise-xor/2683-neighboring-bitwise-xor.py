9bcbedb4681c30ba4a55b8f41cb3c8c1dce3743f
Ki-jong Song <jjongs212@gmail.com>
2025-01-17 19:18:18 +0900

    Time: 15 ms (97.81%), Space: 22.4 MB (48.9%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
@@ -1,6 +1,3 @@
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
-        cumul = 0
-        for x in derived:
-            cumul ^= x
-        return cumul == 0
+        return sum(derived) % 2 == 0
