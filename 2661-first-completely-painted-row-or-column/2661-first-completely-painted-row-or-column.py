e3f011e5536b947583652d42d7caa30154cccf22
Ki-jong Song <jjongs212@gmail.com>
2025-01-20 14:00:01 +0900

    Time: 119 ms (67.19%), Space: 43.8 MB (95.9%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.py
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.py
@@ -1,18 +1,17 @@
+from itertools import product
+
+
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
-        result = size = len(arr)
-        indices = [0] * (size + 1)
-        for i, num in enumerate(arr):
-            indices[num] = i
         m, n = len(mat), len(mat[0])
-        for r in range(m):
-            row_max = 0
-            for c in range(n):
-                row_max = max(row_max, indices[mat[r][c]])
-            result = min(result, row_max)
-        for c in range(n):
-            col_max = 0
-            for r in range(m):
-                col_max = max(col_max, indices[mat[r][c]])
-            result = min(result, col_max)
-        return result
+        pos = [None] * (m * n + 1)
+        for r, c in product(range(m), range(n)):
+            pos[mat[r][c]] = (r, c)
+        row_counts = [0] * m
+        col_counts = [0] * n
+        for i, num in enumerate(arr):
+            r, c = pos[num]
+            row_counts[r] += 1
+            col_counts[c] += 1
+            if row_counts[r] == n or col_counts[c] == m:
+                return i
