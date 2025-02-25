782274c8fd55cb6253988c7c386d8f89b788b166
Ki-jong Song <jjongs212@gmail.com>
2025-02-25 15:17:19 +0900

    Time: 88 ms (37.1%), Space: 21.8 MB (66.67%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.py
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.py
@@ -1,8 +1,11 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
-        n = len(arr)
-        prefix_sums = [0] * (n + 1)
-        for i in range(n):
-            prefix_sums[i + 1] = prefix_sums[i] + arr[i]
-        odd_count = sum(prefix_sum % 2 == 1 for prefix_sum in prefix_sums)
-        return (odd_count * (n + 1 - odd_count)) % (10**9 + 7)
+        result = 0
+        prefix_sum = 0
+        counts = [1, 0]
+        for num in arr:
+            prefix_sum += num
+            is_odd = prefix_sum % 2
+            result += counts[is_odd ^ 1]
+            counts[is_odd] += 1
+        return result % (10**9 + 7)
