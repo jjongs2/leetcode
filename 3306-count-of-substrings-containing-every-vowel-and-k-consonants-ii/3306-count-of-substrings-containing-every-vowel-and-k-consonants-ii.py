7cd9bb062d1cb5dbfe8f4d612465f2bf050ea419
Ki-jong Song <jjongs212@gmail.com>
2025-03-10 17:12:32 +0900

    Time: 1559 ms (78.04%), Space: 18.5 MB (91.33%) - LeetHub

--- 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -2,34 +2,27 @@ class Solution:
     VOWELS = set("aeiou")
 
     def countOfSubstrings(self, word: str, k: int) -> int:
-        p_sums, p_indices = self._make_prefixes(word)
+        return self._helper(word, k) - self._helper(word, k + 1)
+
+    def _helper(self, word, k):
+        n = len(word)
         vowels = dict()
+        consonants = 0
         result = 0
         left = 0
         for right, letter in enumerate(word):
             if letter in self.VOWELS:
                 vowels[letter] = vowels.get(letter, 0) + 1
-            while len(vowels) == 5:
-                p_sum = p_sums[left] + k
-                if p_sum < len(p_indices):
-                    result += max(0, p_indices[p_sum] - max(right, p_indices[p_sum - 1]))
+            else:
+                consonants += 1
+            while len(vowels) == 5 and consonants >= k:
+                result += n - right
                 letter = word[left]
                 if letter in self.VOWELS:
                     vowels[letter] -= 1
                     if vowels[letter] == 0:
                         del vowels[letter]
+                else:
+                    consonants -= 1
                 left += 1
         return result
-
-    def _make_prefixes(self, word):
-        n = len(word)
-        prefix_sums = [0] * (n + 1)
-        prefix_indices = []
-        for i, letter in enumerate(word):
-            prefix_sums[i + 1] = prefix_sums[i]
-            if letter not in self.VOWELS:
-                prefix_sums[i + 1] += 1
-                prefix_indices.append(i)
-        prefix_indices.append(n)
-        prefix_indices.append(0)
-        return prefix_sums, prefix_indices
