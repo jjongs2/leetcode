447baacd91da1e98e1974bdf7518033824b86557
Ki-jong Song <jjongs212@gmail.com>
2026-03-12 21:29:34 +0900

    Time: 0 ms (100%), Space: 8.4 MB (82.62%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int64_t result = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            result = (result * 10) + digit;
+        }
+
+        if (result < INT_MIN || result > INT_MAX)
+            return 0;
+        return result;
+    }
+};
